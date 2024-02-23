21cf5da93555b33864492e1a8fd297ff7ac11b14
sufyan004 <154617555+sufyan004@users.noreply.github.com>
2024-02-24 00:55:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let num1 = prompt("First Value");
+let num2 = prompt("Second Value");
+num1 = Number(num1);
+num2 = Number(num2);
+
+let sum = num1 + num2;
+let minus = num1 - num2;
+let multiply = num1 * num2;
+let divide = num1 / num2;
+
+console.log("Addition : " + sum);
+console.log("Minus : " + minus);
+console.log("Multiplication : " + multiply);
+console.log("Division : " + divide);
+let sufyan;
+sufyan = 50;
+console.log(sufyan);
